c34e8d147f5b0014a31cc26e515e86324dd4a8ff
Papa G <florentgerard@sfr.fr>
2025-03-17 21:08:20 +0100

    création des titres

--- belivemy-premier-projet-react-js/src/App.jsx
+++ belivemy-premier-projet-react-js/src/App.jsx
@@ -1,3 +1,23 @@
+import "./App.css";
+
 export default function App() {
-  return <h1>Hello word !!!</h1>;
+  return (
+    <main>
+      <h1>Marvel</h1>
+      {/* Superhero numéro 1 /> */}
+      <div className="superhero">
+        <h2>Iron Man</h2>
+      </div>
+
+      {/* Superhero numéro 2 /> */}
+      <div className="superhero">
+        <h2>Loki</h2>
+      </div>
+
+      {/* Superhero numéro 3 /> */}
+      <div className="superhero">
+        <h2>Captain America</h2>
+      </div>
+    </main>
+  );
 }
