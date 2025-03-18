85e734293f69233826a9b7d4a93f2f817fcd1655
Papa G <florentgerard@sfr.fr>
2025-03-18 16:32:45 +0100

    ajout des composants

--- belivemy-premier-projet-react-js/src/App.jsx
+++ belivemy-premier-projet-react-js/src/App.jsx
@@ -2,21 +2,28 @@ import "./App.css";
 
 export default function App() {
   return (
-    <main>
+    <main className="superheros">
       <h1>Marvel</h1>
       {/* Superhero numéro 1 /> */}
-      <div className="superhero">
-        <h2>Iron Man</h2>
-      </div>
-
+      <Superhero />
       {/* Superhero numéro 2 /> */}
       <div className="superhero">
         <h2>Loki</h2>
+        <p>
+          Dieu du feu, de la glace, de la magie, du chaos, de la destruction, de
+          la métamorphose et du changement · Il est musclé, beau, intelligent,
+          et drôle.
+        </p>
       </div>
 
       {/* Superhero numéro 3 /> */}
       <div className="superhero">
         <h2>Captain America</h2>
+        <p>
+          Captain America est un super-héros qui a fait ses débuts dans les
+          comics en 1941. Depuis, il est devenu l'un des super-héros les plus
+          populaires de l'univers Marvel.
+        </p>
       </div>
     </main>
   );
---
+++ belivemy-premier-projet-react-js/src/components/superhero/Superhero.jsx
@@ -0,0 +1,11 @@
+export default function Superhero() {
+  return (
+    <div className="superhero">
+      <h2>Iron Man</h2>
+      <p>
+        Anthony « Tony » Stark, alias Iron Man est un super-héros évoluant dans
+        l'univers Marvel de la maison d'édition Marvel Comics.
+      </p>
+    </div>
+  );
+}
