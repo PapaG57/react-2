8113b849a9a739a31a363e4178709055ea4f99a1
Papa G <florentgerard@sfr.fr>
2025-04-25 13:14:02 +0200

    démarrage cours partie 13 (seul 🙈)

--- belyvemy-le-projet-markdown/src/App.jsx
+++ belyvemy-le-projet-markdown/src/App.jsx
@@ -1,3 +1,5 @@
 export default function App() {
-  return <div>Belivemy</div>;
+  return (
+    <textarea defaultValue="# `Prévisualisateur - Believemy`&#13;### Oh ! De la magie !&#13;En réalité, pas vraiment : ceci s'appelle du markdown, et ce projet réalisé entièrement avec React permet de le transformer en *HTML* !&#13;&#13;### Cas d'utilisation&#13;* *italique*&#13;* **gras**&#13;* `monospace`&#13;* ~~barré~~&#13;* # h1&#13;* ## h2&#13;* ### h3&#13;* #### etc&#13;[Believemy](https://believemy.com)"></textarea>
+  );
 }
