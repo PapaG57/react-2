251689ac9245fd391a9f37a5778faa17dc0b3423
Papa G <florentgerard@sfr.fr>
2025-04-25 16:52:27 +0200

    corrections cours Partie 13

--- belyvemy-le-projet-markdown/src/App.jsx
+++ belyvemy-le-projet-markdown/src/App.jsx
@@ -1,5 +1,47 @@
+// Librairies
+import { useState, useEffect, useRef } from "react";
+
+// Composant
+import Markdown from "marked-react";
+
 export default function App() {
+  // Variable
+  const input = useRef();
+
+  // State
+  const [prev, setPrev] = useState("");
+
+  // Fonction
+  const updatePrevisualisation = () => {
+    setPrev(input.current.value);
+  };
+
+  // Cycle
+  useEffect(() => {
+    if (input.current) {
+      input.current.focus();
+    }
+    updatePrevisualisation();
+  }, []);
+
   return (
-    <textarea defaultValue="# `Prévisualisateur - Believemy`&#13;### Oh ! De la magie !&#13;En réalité, pas vraiment : ceci s'appelle du markdown, et ce projet réalisé entièrement avec React permet de le transformer en *HTML* !&#13;&#13;### Cas d'utilisation&#13;* *italique*&#13;* **gras**&#13;* `monospace`&#13;* ~~barré~~&#13;* # h1&#13;* ## h2&#13;* ### h3&#13;* #### etc&#13;[Believemy](https://believemy.com)"></textarea>
+    <div className="App">
+      <div className="elements">
+        {/* Textarea */}
+        <div className="element">
+          <textarea
+            rows={30}
+            onChange={updatePrevisualisation}
+            ref={input}
+            defaultValue="# `Prévisualisateur - Believemy`&#13;### Oh ! De la magie !&#13;En réalité, pas vraiment : ceci s'appelle du markdown, et ce projet réalisé entièrement avec React permet de le transformer en *HTML* !&#13;&#13;### Cas d'utilisation&#13;* *italique*&#13;* **gras**&#13;* `monospace`&#13;* ~~barré~~&#13;* # h1&#13;* ## h2&#13;* ### h3&#13;* #### etc&#13;[Believemy](https://believemy.com)"
+          ></textarea>
+        </div>
+
+        {/* Prévisualisateur */}
+        <div className="element">
+          <Markdown>{prev}</Markdown>
+        </div>
+      </div>
+    </div>
   );
 }
